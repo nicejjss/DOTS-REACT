250a1d6cb0a4e66466ae4a21b346f14a8663da98
nicejjss <locdaoduc2002@gmail.com>
2025-06-14 19:24:19 +0700

    config for vercel

--- src/constants/Music.jsx
+++ src/constants/Music.jsx
@@ -1,14 +1,14 @@
 export const backgroundMusic = {
   name: 'background-music',
-  src: '/public/audio/background.mp3',
+  src: '/audio/background.mp3',
 };
 
 export const dotMusic = {
   name: 'dot-music',
-  src: '/public/audio/dot.mp3',
+  src: '/audio/dot.mp3',
 };
 
 export const victory = {
   name: 'victory',
-  src: '/public/audio/victory.mp3',
+  src: '/audio/victory.mp3',
 };
